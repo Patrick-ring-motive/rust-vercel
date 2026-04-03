814267ce12ff19ea2a03aa4d469e595dfec04781
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 19:12:01 -0500

    style: js-beautify formatting (test/fixtures.test.ts)

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -1,4 +1,6 @@
-import { join } from 'node:path';
+import {
+  join
+} from 'node:path';
 import ms from 'ms';
 import execa from 'execa';
 import fetch from 'node-fetch';
@@ -9,24 +11,24 @@ jest.setTimeout(ms('50m'));
 
 interface Probe {
   path: string;
-  status?: number;
-  mustContain?: string;
+  status ? : number;
+  mustContain ? : string;
 }
 
 interface ProbesConf {
   probes: Probe[];
 }
 
-async function importJSON<T>(path: string): Promise<T> {
-  return (await import(path)) as unknown as Promise<T>;
+async function importJSON < T > (path: string): Promise < T > {
+  return (await import(path)) as unknown as Promise < T > ;
 }
 
-async function checkProbes(baseUrl: string, probes: Probe[]): Promise<void> {
+async function checkProbes(baseUrl: string, probes: Probe[]): Promise < void > {
   for (const probe of probes) {
     // eslint-disable-next-line no-await-in-loop
     const response = (await fetch(`${baseUrl}${probe.path}`)) as unknown as {
       status: number;
-      text: () => Promise<string>;
+      text: () => Promise < string > ;
     };
 
     const status = response.status;
@@ -53,7 +55,7 @@ function getVercelProcess(dir: string): execa.ExecaChildProcess {
   return execa('vercel', [...defaultArgs, dir]);
 }
 
-function isReady(vercelServ: execa.ExecaChildProcess): Promise<string> {
+function isReady(vercelServ: execa.ExecaChildProcess): Promise < string > {
   return new Promise((resolve) => {
     vercelServ.stderr?.on('data', (d: Buffer) => {
       const res = readyRegex.exec(d.toString());
@@ -65,10 +67,12 @@ function isReady(vercelServ: execa.ExecaChildProcess): Promise<string> {
   });
 }
 
-async function testFixture(fixture: string): Promise<'ok'> {
+async function testFixture(fixture: string): Promise < 'ok' > {
   /* await injectConf(join(__dirname, 'fixtures', fixture)); */
 
-  const { probes } = await importJSON<ProbesConf>(
+  const {
+    probes
+  } = await importJSON < ProbesConf > (
     join(__dirname, 'fixtures', fixture, 'probes.json'),
   );
 
